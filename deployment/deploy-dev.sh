a66d59bd3651bc628af82d0542356b05ef99059a
petrosrapto <petrosrapto@gmail.com>
2025-11-24 23:12:36 +0200

    fixes

--- deployment/deploy-dev.sh
+++ deployment/deploy-dev.sh
@@ -118,7 +118,7 @@ services:
       CELERY_BROKER_URL: "amqp://rabbitmq:5672"
       CELERY_RESULT_BACKEND: "redis://redis:6379/0"
       SERVICE_NAME: "multiagentframework_service"
-      DATABASE_URL: "postgresql://\${POSTGRES_USER}:\${POSTGRES_PASSWORD}@postgres:5432/\${POSTGRES_DB}"
+      DATABASE_URL: "postgresql://\$\${POSTGRES_USER}:\$\${POSTGRES_PASSWORD}@postgres:5432/\$\${POSTGRES_DB}"
       PYTHONUNBUFFERED: "1"
     ports:
       - "5001:5001"
@@ -151,7 +151,7 @@ services:
       CELERY_BROKER_URL: "amqp://rabbitmq:5672"
       CELERY_RESULT_BACKEND: "redis://redis:6379/0"
       SERVICE_NAME: "multiagentframework_service"
-      DATABASE_URL: "postgresql://\${POSTGRES_USER}:\${POSTGRES_PASSWORD}@postgres:5432/\${POSTGRES_DB}"
+      DATABASE_URL: "postgresql://\$\${POSTGRES_USER}:\$\${POSTGRES_PASSWORD}@postgres:5432/\$\${POSTGRES_DB}"
       PYTHONUNBUFFERED: "1"
     restart: always
     volumes:
